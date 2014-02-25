b0488457fa4414933fc66daf8038fb8646302a70
stoychevvasko <stoychev.vassil@gmail.com>
2014-02-25 07:43:04 +0200

    sound event update

---
+++ KurtVonnegut/GameStateManagementSample/GameObject/Sound/SoundCaller/SoundCaller.cs
@@ -0,0 +1,27 @@
+﻿namespace GameStateManagementSample
+{
+    using System;
+    using System.Threading;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Audio;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    public class SoundCaller : Sound
+    {
+        SoundPublisher publisher;
+        SoundSubscriber subscriber;
+        
+        public SoundCaller(SoundEffect eff)
+            :base()
+        {
+            publisher = new SoundPublisher(eff);
+            subscriber = new SoundSubscriber();
+            subscriber.Subscribe(publisher);
+            publisher.Execute();
+        }
+
+        
+    }
+}
