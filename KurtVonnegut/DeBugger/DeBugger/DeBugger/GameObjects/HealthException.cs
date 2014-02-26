2069007c757d1377eefd051ef61f56bb74b3dd08
sob4o <ferrari1989@gmail.com>
2014-02-26 15:44:29 +0200

    Added exception class

---
+++ KurtVonnegut/DeBugger/DeBugger/DeBugger/GameObjects/HealthException.cs
@@ -0,0 +1,17 @@
+﻿namespace DeBugger.GameObjects
+{
+    using System;
+
+
+    public class HealthException<T> : Exception
+    {
+        public T Start { get; set; }
+        public T End { get; set; }
+        public HealthException(string msg, T start, T end)
+            : base(msg)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
